54abf7af78c0ad91034779e434b4f913c8b3886e
Max <maksim@sadovskij.com>
2019-08-07 11:47:31 +0300

    Implement POST /tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,4 +3,25 @@ class Api::V1::TasksController < ApplicationController
     tasks = Task.all
     render json: tasks
   end
+  
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: task
+    else
+      render json: {
+        errors: task.errors.map do |attr, error|
+          {
+            status: 422,
+            title: "\"#{attr.to_s}\" #{error}"
+          }
+        end
+      }, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  def task_params
+    params.require(:data).require(:attributes).permit(:title)
+  end
 end
--- spec/requests/tasks_controller_spec.rb
+++ spec/requests/tasks_controller_spec.rb
@@ -25,4 +25,40 @@ RSpec.describe "Tasks API", type: :request do
       # expect(parsed_json['data'].second).to eql(titles.second)
     end
   end
+  
+  describe "POST /tasks" do
+    context "when title is present" do
+      let(:valid_attrs) {{title: Faker::Superhero.name}}
+      before {post '/api/v1/tasks', params: {data: {attributes: valid_attrs}}}
+      
+      it "returns status code 201" do
+        expect(response.content_type).to eq("application/json")
+        expect(response).to have_http_status(:ok)
+      end
+      
+      it "returns created Task" do
+        json['data'].tap do |data|
+          expect(data['id'].to_i).to be > 0
+          expect(data['type']).to eql('tasks')
+          expect(data['attributes']['title']).to eql(Task.first.title)
+        end
+      end
+    end
+    
+    context "when data is invalid" do
+      after do
+        expect(Task.count).to eq 0
+      end
+      
+      it "rejects Task with missing title" do
+        expect{post '/api/v1/tasks', params: {data: {attributes: {}}}}.to raise_error(ActionController::ParameterMissing)
+      end
+  
+      it "rejects Task with blank title" do
+        post '/api/v1/tasks', params: {data: {attributes: {title: ''}}}
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json['errors'].first['title']).to eql('"title" can\'t be blank')
+      end
+    end
+  end
 end
