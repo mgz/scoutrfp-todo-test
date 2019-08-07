d38829f20dc738df1d9b611214ed17bf10d04b8c
Max <maksim@sadovskij.com>
2019-08-07 10:47:06 +0300

    Implement GET /tasks

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module TodoApiApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.paths.add "app/serializers", eager_load: true
   end
 end
